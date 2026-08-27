bde6c4050a990d2c7e8ce71caaee79efd62be2e9
Guandaru-Kabugi <kabugi96@gmail.com>
2026-08-27 08:18:22 +0300

    I have started  integrating  routes into routes files

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-import express from 'express';
+import express, { json } from 'express';
 import cors from 'cors';
 
 import dotenv from 'dotenv';
+import { errorHandler } from './middleware/errorhandler.js';
 
-dotenv.config()
+import router from './routes/ideasroutes.js';
+
+dotenv.config();
 
 const app = express();
 
@@ -11,6 +14,22 @@ const app = express();
 const PORT = process.env.PORT|| 8000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//routes
+app.use('/api/ideas', router);
+
+//404 fall back
+
+app.use((req, res, next) => {
+    const error = new Error(`Not found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+})
+
+app.use(errorHandler)
+
 
 app.listen(PORT, () =>{
     console.log(`server running on port ${PORT}`);
